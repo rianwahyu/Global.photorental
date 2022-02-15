667fb21110b193cb6ddf8e703dcadecd071af67d
Rian Wahyu <rianwahyu26@gmail.com>
2022-02-15 11:45:45 +0700

    fix delete ORder

--- config/order/deleteOrder.php
+++ config/order/deleteOrder.php
@@ -4,11 +4,13 @@ include '../connection.php';
 
 $order_id = $_POST['order_id'];
 
-$query = "DELETE FROM `order_tbl` WHERE `order_id`='$order_id' ";
+$query = "";
+$query = $query . "DELETE FROM `order_tbl` WHERE `order_id`='$order_id' ; ";
+$query = $query . "DELETE FROM `order_value_tbl` WHERE `order_id`='$order_id'; ";
 //echo $query;
 
-$result = mysqli_query($dbc, $query);
+$result = mysqli_multi_query($dbc, $query);
 
-if($result == true){
+if ($result == true) {
     echo "<meta http-equiv='refresh' content='1 url=../../order_list'>";
 }
